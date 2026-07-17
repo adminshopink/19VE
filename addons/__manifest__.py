6159af1c493840149ea3d6ca23af6984c1933ee8
adminshopink <admin.shopink@gmail.com>
2026-07-17 13:42:42 -0400

    Create __manifest__.py

---
+++ addons/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Base Setup Shopink',
+    'version': '1.0',
+    'category': 'Hidden',
+    'summary': 'Configuración base de módulos core',
+    'license': 'LGPL-3',
+    'depends': [
+        'crm',
+        'sale_management',
+        'stock',
+        'account_accountant',
+        'web_studio',
+        'purchase',
+    ],
+    'installable': True,
+    'application': True,
+}
