93de1a2e8578d6377a4258c974c3b71a9c33c853
adminshopink <admin.shopink@gmail.com>
2026-07-19 10:13:17 -0400

    Update account_payment.py

--- addons/shopink_sale_commercial_payment/models/account_payment.py
+++ addons/shopink_sale_commercial_payment/models/account_payment.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
@@ -5,11 +8,13 @@ class AccountPayment(models.Model):
         # Ejecutamos el posteo original
         res = super(AccountPayment, self).action_post()
         
-        # Después de postear, forzamos la actualización de la orden
+        # Después de confirmar, forzamos la actualización de la orden
         for payment in self:
             if payment.memo:
+                # Buscamos la orden que coincida con el memo
                 order = self.env['sale.order'].search([('name', 'ilike', payment.memo)], limit=1)
                 if order:
+                    # Invalidamos y recalculamos
                     order.invalidate_recordset(['commercial_payment_state'])
                     order.recompute(['commercial_payment_state'])
         return res
