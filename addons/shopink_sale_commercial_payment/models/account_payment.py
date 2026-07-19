3223d642c01f023fc016cea0413fe4dbb83e2e7c
adminshopink <admin.shopink@gmail.com>
2026-07-19 11:07:13 -0400

    Update account_payment.py

--- addons/shopink_sale_commercial_payment/models/account_payment.py
+++ addons/shopink_sale_commercial_payment/models/account_payment.py
@@ -1,11 +1,15 @@
-def action_post(self):
+# -*- coding: utf-8 -*-
+from odoo import models
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    def action_post(self):
         res = super(AccountPayment, self).action_post()
         for payment in self:
-            # BUSCAMOS POR EL CAMPO 'ref' (Referencia) que es donde Odoo pone el memo
-            search_term = payment.ref or payment.memo
-            if search_term:
-                order = self.env['sale.order'].search([('name', 'ilike', search_term)], limit=1)
-                if order:
-                    order.invalidate_recordset(['commercial_payment_state', 'last_payment_memo', 'last_payment_journal'])
-                    order.recompute(['commercial_payment_state', 'last_payment_memo', 'last_payment_journal'])
+            # Buscamos la orden que coincida con la referencia del pago
+            order = self.env['sale.order'].search([('name', 'ilike', payment.ref)], limit=1)
+            if order:
+                # Forzamos la actualización de los campos computados
+                order._compute_payment_info()
         return res
