8eb3094d5068f46f48ec808c6e924efb2f52063c
adminshopink <admin.shopink@gmail.com>
2026-07-19 10:05:20 -0400

    Update account_payment.py

--- addons/shopink_sale_commercial_payment/models/account_payment.py
+++ addons/shopink_sale_commercial_payment/models/account_payment.py
@@ -5,17 +5,16 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     def write(self, vals):
-        # Capturamos el estado antes de guardar los cambios
-        # Si el valor que viene en 'vals' cambia el estado a 'posted'
+        # Ejecutamos la escritura original primero
         res = super(AccountPayment, self).write(vals)
         
-        if 'state' in vals and vals['state'] == 'paid':
+        # CAMBIO: Usamos 'posted' que es el estado real de los pagos confirmados
+        if 'state' in vals and vals['state'] == 'posted':
             for payment in self:
-                # Buscamos la orden que coincida con el memo
                 if payment.memo:
                     order = self.env['sale.order'].search([('name', 'ilike', payment.memo)], limit=1)
                     if order:
-                        # Forzamos el recálculo del campo computado
+                        # Forzamos la actualización de la orden
+                        order.invalidate_recordset(['commercial_payment_state'])
                         order.recompute(['commercial_payment_state'])
-                        
         return res
