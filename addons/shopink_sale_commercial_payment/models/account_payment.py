03924cb7ac46733a0e8de9e150239721df91af35
adminshopink <admin.shopink@gmail.com>
2026-07-19 11:24:19 -0400

    Update account_payment.py

--- addons/shopink_sale_commercial_payment/models/account_payment.py
+++ addons/shopink_sale_commercial_payment/models/account_payment.py
@@ -7,12 +7,9 @@ class AccountPayment(models.Model):
     def action_post(self):
         res = super(AccountPayment, self).action_post()
         for payment in self:
-            # Buscamos la orden que coincida con el memo (en Odoo el campo es 'ref' o 'name')
-            # Usamos 'name' del pago porque ahí suele estar la referencia bancaria
+            # Buscamos usando 'name' que es el campo estándar de referencia
             order = self.env['sale.order'].search([('name', 'ilike', payment.ref or payment.name)], limit=1)
-            
             if order:
-                # Escribimos directo en los campos de la orden (sin funciones compute)
                 order.write({
                     'last_payment_memo': payment.ref or payment.name,
                     'last_payment_journal': payment.journal_id.name,
