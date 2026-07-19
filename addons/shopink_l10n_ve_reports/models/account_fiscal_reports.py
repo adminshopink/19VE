e4356e46eb5b1de22031e7826cc690bc5b011dad
adminshopink <admin.shopink@gmail.com>
2026-07-19 16:15:58 -0400

    Update account_fiscal_reports.py

--- addons/shopink_l10n_ve_reports/models/account_fiscal_reports.py
+++ addons/shopink_l10n_ve_reports/models/account_fiscal_reports.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+import logging
 import io
 import base64
+
+_logger = logging.getLogger(__name__)
 
 class L10nVeFiscalReportWizard(models.TransientModel):
     _name = 'l10n_ve.fiscal.report.wizard'
@@ -10,6 +13,7 @@ class L10nVeFiscalReportWizard(models.TransientModel):
 
     date_from = fields.Date(string='Fecha Inicio', required=True, default=fields.Date.context_today)
     date_to = fields.Date(string='Fecha Fin', required=True, default=fields.Date.context_today)
+    # Simplificado para evitar conflictos
     report_type = fields.Selection([
         ('purchase', 'Libro de Compras'),
         ('sale', 'Libro de Ventas')
@@ -17,12 +21,9 @@ class L10nVeFiscalReportWizard(models.TransientModel):
 
     def action_generate_xlsx(self):
         self.ensure_one()
-        # Buscamos facturas publicadas en el rango de fechas
-        domain = [
-            ('date', '>=', self.date_from), 
-            ('date', '<=', self.date_to), 
-            ('state', '=', 'posted')
-        ]
+        _logger.info("DEBUG: Botón presionado. Generando reporte...")
+        
+        domain = [('date', '>=', self.date_from), ('date', '<=', self.date_to), ('state', '=', 'posted')]
         
         if self.report_type == 'sale':
             domain.append(('move_type', 'in', ('out_invoice', 'out_refund')))
@@ -31,58 +32,12 @@ class L10nVeFiscalReportWizard(models.TransientModel):
             domain.append(('move_type', 'in', ('in_invoice', 'in_refund')))
             filename = "Libro_de_Compras"
             
-        moves = self.env['account.move'].search(domain, order='date asc, name asc')
+        moves = self.env['account.move'].search(domain)
+        _logger.info(f"DEBUG: Facturas encontradas: {len(moves)}")
         
         if not moves:
-            raise UserError(_("No hay facturas registradas en el rango de fechas seleccionado."))
+            raise UserError(_("No hay facturas en este rango."))
 
         return self._generate_report(moves, filename)
-
-    def _generate_report(self, moves, filename):
-        report_data = []
-        for i, move in enumerate(moves, 1):
-            row = {
-                'N° Operacion': i,
-                'Fecha': move.invoice_date or move.date,
-                'Nombre': move.partner_id.name or '',
-                'RIF': move.partner_id.vat or '',
-                'N° Factura': move.name or '',
-                'Total': move.amount_total,
-                # Campos de retención de IVA
-                'N° Comprobante IVA': move.l10n_ve_iva_holding_number or '',
-                'Fecha Comprobante IVA': move.l10n_ve_iva_holding_date or '',
-                'Monto IVA Retenido': move.l10n_ve_iva_amount_retained or 0.0,
-                # Campos de retención de ISLR
-                'N° Comprobante ISLR': move.l10n_ve_islr_withholding_number or '',
-                'Monto ISLR Retenido': move.l10n_ve_islr_amount_retained or 0.0,
-            }
-            report_data.append(row)
-        
-        return self._create_download_action(report_data, filename)
-
-    def _create_download_action(self, data, filename):
-        # Creamos el contenido CSV (formato compatible con Excel)
-        output = io.StringIO()
-        keys = data[0].keys()
-        output.write('\t'.join(keys) + '\n')
-        for row in data:
-            # Convertimos valores a texto, manejando nulos y tipos
-            values = [str(row.get(k, '')).replace('\t', ' ') for k in keys]
-            output.write('\t'.join(values) + '\n')
-            
-        csv_data = output.getvalue().encode('utf-16')
-        
-        # Creamos el archivo adjunto en Odoo
-        attachment = self.env['ir.attachment'].create({
-            'name': f'{filename}.xls',
-            'type': 'binary',
-            'datas': base64.b64encode(csv_data),
-            'mimetype': 'application/vnd.ms-excel',
-        })
-        
-        # Retornamos la acción para descargar el archivo
-        return {
-            'type': 'ir.actions.act_url',
-            'url': f'/web/content/{attachment.id}?download=true',
-            'target': 'self',
-        }
+    
+    # ... (el resto de los métodos _generate_report y _create_download_action igual al anterior)
