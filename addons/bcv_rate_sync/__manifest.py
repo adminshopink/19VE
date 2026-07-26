91f6481f099c7d784bc47215ac94b83ba61741de
adminshopink <admin.shopink@gmail.com>
2026-07-26 12:08:12 -0400

    Create __manifest.py

---
+++ addons/bcv_rate_sync/__manifest.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sincronización Tasa BCV Shopink',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Actualiza automáticamente la tasa del USD desde Google Sheets para Venezuela',
+    'depends': ['base', 'account'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
