a306fd1eea5a74eaedd3c461122ca2008fcfa6cb
adminshopink <admin.shopink@gmail.com>
2026-07-31 21:53:43 -0400

    Update __manifest__.py

--- addons/shopink_sale_commercial_payment/__manifest__.py
+++ addons/shopink_sale_commercial_payment/__manifest__.py
@@ -9,8 +9,14 @@
     'data': [
         'security/ir.model.access.csv',
         'views/sale_order_views.xml',
-        'wizard/sale_commercial_report_wizard_views.xml', # Añade esta línea
+        'wizard/sale_commercial_report_wizard_views.xml',
     ],
+    # Aquí insertamos la sección de assets
+    'assets': {
+        'web.assets_backend': [
+            'shopink_sale_commercial_payment/static/src/css/hide_save.css',
+        ],
+    },
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
